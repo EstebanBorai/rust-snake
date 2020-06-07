e1187647f4f22f0d8f91c274826a97cb32c27620
Esteban Borai <estebanborai@gmail.com>
2020-06-07 15:24:55 -0300

    Working on Game State

---
+++ src/util/colors.rs
@@ -0,0 +1,6 @@
+use piston_window::types::Color;
+
+pub const BLACK: Color = [0.00, 0.00, 0.00, 1.0];
+pub const BLUE: Color = [0.00, 0.00, 1.00, 1.0];
+pub const GREEN: Color = [0.00, 1.00, 0.00, 1.0];
+pub const RED: Color = [1.00, 0.00, 0.00, 1.0];
